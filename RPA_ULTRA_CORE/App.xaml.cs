1a8f6873e7ae4cb3436a3891aba5a75d5165a37e
SergioSilva77 <ssergiosilva44@gmail.com>
2025-10-16 21:30:38 -0300

    Adicionar arquivos de projeto.

---
+++ RPA_ULTRA_CORE/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RPA_ULTRA_CORE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
